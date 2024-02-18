35e15089f463bcb63b6e5f2a9d8ef8a84bab3e0a
Martin K <mk_alternativ@posteo.de>
2024-02-18 21:27:12 +0100

    edge: turn structs into classes

--- src/edge.hpp
+++ src/edge.hpp
@@ -2,15 +2,39 @@
 
 #include "types.hpp"
 
-struct Edge {
-    VertexID endpoints[2];
+class Edge {
+protected:
+    VertexID mEndpoints[2];
+
+public:
+    Edge(VertexID endpoint0, VertexID endpoint1) {
+        mEndpoints[0] = endpoint0;
+        mEndpoints[1] = endpoint1;
+    }
+
+    VertexID endpoint(int index) const { return mEndpoints[index]; }
+    void setEndpoint(int index, VertexID endpoint) { mEndpoints[index] = endpoint; }
 };
 
-struct WeightedEdge : public Edge {
-    EdgeWeight weight;
+class WeightedEdge : public Edge {
+private:
+    EdgeWeight mWeight;
+
+public:
+    WeightedEdge(VertexID endpoint0, VertexID endpoint1, EdgeWeight weight) : Edge(endpoint0, endpoint1), mWeight(weight) {}
+
+    EdgeWeight weight() const { return mWeight; }
+    void setWeight(EdgeWeight weight) { mWeight = weight; }
 };
 
-struct MultiEdge : public Edge {
-    EdgeMultiplicity multiplicity;
+class MultiEdge : public Edge {
+private:
+    EdgeMultiplicity mMultiplicity;
+
+public:
+    MultiEdge(VertexID endpoint0, VertexID endpoint1, EdgeMultiplicity multiplicity) : Edge(endpoint0, endpoint1), mMultiplicity(multiplicity) {}
+
+    EdgeMultiplicity multiplicity() const { return mMultiplicity; }
+    void setMultiplicity(EdgeMultiplicity multiplicity) { mMultiplicity = multiplicity; }
 };
 
