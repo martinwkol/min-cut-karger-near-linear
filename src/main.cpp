505598196d1ccbecbed76171e6419904d92ce42e
Martin K <mk_alternativ@posteo.de>
2024-04-08 02:07:31 +0200

    write main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,47 @@
+#include <iostream>
+#include <limits>
+
+#include "graph.hpp"
+#include "two_respecting_trees.hpp"
+#include "two_respecting_cuts.hpp"
+
+WeightedGraph readWeightedGraph() {
+    size_t numVertices;
+    size_t numEdges;
+    std::cin >> numVertices >> numEdges;
+
+    std::vector<WeightedEdge> edges;
+    edges.reserve(numEdges);
+
+    for (size_t i = 0; i < numEdges; i++) {
+        VertexID endpoint0;
+        VertexID endpoint1;
+        EdgeWeight weight;
+
+        std::cin >> endpoint0 >> endpoint1 >> weight;
+        edges.emplace_back(endpoint0, endpoint1, weight);
+    }
+
+    return WeightedGraph(numVertices, std::move(edges));
+}
+
 int main() {
-    
+    WeightedGraph graph = readWeightedGraph();
+    TreePacking packing = findTwoRespectingTrees(graph, 3);
+    std::vector<VertexID> minCut;
+    EdgeWeight minCutWeight = INFINITE_WEIGHT;
+    for (const std::vector<size_t>& edgeSelection : packing.trees()) {
+        RootedSpanningTree rst(graph, edgeSelection, 0);
+        auto smallestCut = findSmallest2RespectingCut(rst);
+        if (smallestCut.second < minCutWeight) {
+            minCutWeight = smallestCut.second;
+            minCut = std::move(smallestCut.first);
+        }
+    }
+    std::cout << minCutWeight << std::endl;
+    for (VertexID vertex : minCut) {
+        std::cout << vertex << '\n';
+    }
+
+    return 0;
 }
--- src/types.hpp
+++ src/types.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <limits>
+
 typedef unsigned int uint;
 typedef unsigned long long ull;
-
 
 typedef unsigned int VertexID;
 typedef double EdgeWeight;
 typedef ull EdgeMultiplicity;
+
+constexpr static EdgeWeight INFINITE_WEIGHT = std::numeric_limits<EdgeWeight>::max();
