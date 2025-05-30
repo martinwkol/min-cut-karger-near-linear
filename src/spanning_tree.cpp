170ea9c6a200eb3bdd5b8a67ddd8373d2126dd4a
Martin Koloseus <mkoloseus@posteo.de>
2025-05-30 13:35:34 +0200

    refactor(spanning tree): improve structure & naming & documentation

--- src/spanning_tree.cpp
+++ src/spanning_tree.cpp
@@ -6,56 +6,35 @@
 #include <stdexcept>
 #include "union_find.hpp"
 
-template <bool MAX_SPANNING_TREE, bool USE_WEIGHTS>
-static bool spanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights) {
-    if (graph.numVertices() <= 1) {
-        return true;
-    }
-    if (edgeIndicesSize < graph.numVertices() - 1) {
-        throw std::runtime_error("edgeIndices too small");
-    }
-
+template <bool MAX, bool USE_WEIGHTS>
+static bool spanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights) {
     const std::vector<MultiEdge>& edges = graph.edges();
-    const std::vector<MultiEdge>* edgesPtr = &edges;
     
+    if (bufferSize < graph.numVertices() - 1) return false;
+    if (USE_WEIGHTS && edges.size() != numWeights) return false;
+    if (graph.numVertices() <= 1) return true;
 
+    std::vector<size_t> ordering(edges.size());
+    std::iota(ordering.begin(), ordering.end(), 0);
     if constexpr(USE_WEIGHTS) {
-        if (edges.size() != numWeights) {
-            throw std::runtime_error("the number of edges does not match the number of edges");
-        }
-    }
-
-    std::vector<size_t> sortedIndices(edges.size());
-    std::iota(sortedIndices.begin(), sortedIndices.end(), 0);
-
-    if constexpr(USE_WEIGHTS && MAX_SPANNING_TREE) {
-        std::sort(sortedIndices.begin(), sortedIndices.end(), [weights](size_t i0, size_t i1) {
-            return weights[i0] > weights[i1];
+        std::sort(ordering.begin(), ordering.end(), [weights](size_t i0, size_t i1) {
+            if constexpr(MAX)   return weights[i0] > weights[i1];
+            else                return weights[i0] < weights[i1];
         });
-    }
-    if constexpr(USE_WEIGHTS && !MAX_SPANNING_TREE) {
-        std::sort(sortedIndices.begin(), sortedIndices.end(), [weights](size_t i0, size_t i1) {
-            return weights[i0] < weights[i1];
-        });
-    }
-    if constexpr(!USE_WEIGHTS && MAX_SPANNING_TREE) {
-        std::sort(sortedIndices.begin(), sortedIndices.end(), [edgesPtr](size_t i0, size_t i1) {
-            return (*edgesPtr)[i0].multiplicity() > (*edgesPtr)[i1].multiplicity();
-        });
-    }
-    if constexpr(!USE_WEIGHTS && !MAX_SPANNING_TREE) {
-        std::sort(sortedIndices.begin(), sortedIndices.end(), [edgesPtr](size_t i0, size_t i1) {
-            return (*edgesPtr)[i0].multiplicity() < (*edgesPtr)[i1].multiplicity();
+    } else {
+        std::sort(ordering.begin(), ordering.end(), [&edges](size_t i0, size_t i1) {
+            if constexpr(MAX)   return edges[i0].multiplicity() > edges[i1].multiplicity();
+            else                return edges[i0].multiplicity() < edges[i1].multiplicity();
         });
     }
 
     UnionFind uf(graph.numVertices());
     size_t numSelectedEdges = 0;
-    for (size_t index : sortedIndices) {
+    for (size_t index : ordering) {
         const MultiEdge& edge = edges[index];
 
         if (uf.find(edge.endpoint(0)) != uf.find(edge.endpoint(1))) {
-            *edgeIndices++ = index;
+            *edgeIndicesBuffer++ = index;
             numSelectedEdges++;
 
             if (numSelectedEdges == graph.numVertices() - 1) {
@@ -66,21 +45,22 @@ static bool spanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const Mult
         }
     }
 
-    throw false;
+    return false;
 } 
 
-bool minSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph) {
-    return spanningTree<false, false>(edgeIndices, edgeIndicesSize, graph, nullptr, 0);
+
+bool minSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph) {
+    return spanningTree<false, false>(edgeIndicesBuffer, bufferSize, graph, nullptr, 0);
 }
 
-bool minSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights) {
-    return spanningTree<false, true>(edgeIndices, edgeIndicesSize, graph, weights, numWeights);
+bool minSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights) {
+    return spanningTree<false, true>(edgeIndicesBuffer, bufferSize, graph, weights, numWeights);
 }
 
-bool maxSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph) {
-    return spanningTree<true, false>(edgeIndices, edgeIndicesSize, graph, nullptr, 0);
+bool maxSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph) {
+    return spanningTree<true, false>(edgeIndicesBuffer, bufferSize, graph, nullptr, 0);
 }
 
-bool maxSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights) {
-    return spanningTree<true, true>(edgeIndices, edgeIndicesSize, graph, weights, numWeights);
+bool maxSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights) {
+    return spanningTree<true, true>(edgeIndicesBuffer, bufferSize, graph, weights, numWeights);
 }
--- src/spanning_tree.hpp
+++ src/spanning_tree.hpp
@@ -4,31 +4,62 @@
 #include "graph.hpp"
 
 /**
- * Compute the minimum spanning tree with respect to the edge-multiplicities
- * @param edgeIndices pointer to the location where the indices of the edges in the spanning tree will be stored
- * @return true if success, false if graph disconnected
-*/
-bool minSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph);
+ * Compute the minimum spanning tree (MST) of the given graph based on edge multiplicities.
+ * @param edgeIndicesBuffer Pointer to a pre-allocated buffer where the indices of the edges forming 
+ *        the MST will be written. The buffer must have space for at least (numVertices - 1) edges.
+ * @param bufferSize The number of entries available in edgeIndicesBuffer.
+ * @param graph The input MultiGraph from which the MST will be computed.
+ * @return true if the spanning tree was successfully computed.
+ *         false if:
+ *           - the graph is disconnected,
+ *           - the bufferSize is too small.
+ */
+bool minSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph);
 
 /**
- * Compute the minimum spanning tree with respect to weights from the weight-list
- * @param edgeIndices pointer to the location where the indices of the edges in the spanning tree will be stored
- * @return true if success, false if graph disconnected
-*/
-bool minSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights);
+ * Compute the minimum spanning tree (MST) using explicit edge weights.
+ *
+ * @param edgeIndicesBuffer Pointer to a pre-allocated buffer to store the indices of the MST edges.
+ *        Must hold at least (numVertices - 1) entries.
+ * @param bufferSize Number of entries available in edgeIndicesBuffer.
+ * @param graph The input MultiGraph to process.
+ * @param weights Array of edge weights, where weights[i] corresponds to edge i in the graph.
+ * @param numWeights The number of weights provided. Must exactly match the number of edges in the graph.
+ * @return true if the spanning tree was successfully computed;
+ *         false if:
+ *           - the graph is disconnected,
+ *           - bufferSize is too small,
+ *           - numWeights does not match the number of edges in the graph.
+ */
+bool minSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights);
 
 /**
- * Compute the maximum spanning tree with respect to the edge-multiplicities
- * @param edgeIndices pointer to the location where the indices of the edges in the spanning tree will be stored
- * @return true if success, false if graph disconnected
-*/
-bool maxSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph);
+ * Compute the maximum spanning tree (MaxST) of the graph using edge multiplicities.
+ *
+ * @param edgeIndicesBuffer Pointer to a pre-allocated buffer to store the indices of the MaxST edges.
+ * @param bufferSize Number of entries available in edgeIndicesBuffer. Should be at least (numVertices - 1).
+ * @param graph The input MultiGraph to process.
+ * @return true if the maximum spanning tree was successfully computed;
+ *         false if:
+ *           - the graph is disconnected,
+ *           - bufferSize is too small.
+ */
+bool maxSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph);
 
 /**
- * Compute the maximum spanning tree with respect to weights from the weight-list
- * @param edgeIndices pointer to the location where the indices of the edges in the spanning tree will be stored
- * @return true if success, false if graph disconnected
-*/
-bool maxSpanningTree(size_t* edgeIndices, size_t edgeIndicesSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights);
+ * Compute the maximum spanning tree (MaxST) using explicit edge weights.
+ *
+ * @param edgeIndicesBuffer Pointer to a pre-allocated buffer to store the indices of the MaxST edges.
+ * @param bufferSize Number of entries available in edgeIndicesBuffer.
+ * @param graph The input MultiGraph.
+ * @param weights Array of edge weights (type double), where weights[i] corresponds to edge i.
+ * @param numWeights The number of weights provided. Must match the number of edges in the graph.
+ * @return true if the maximum spanning tree was successfully computed;
+ *         false if:
+ *           - the graph is disconnected,
+ *           - bufferSize is too small,
+ *           - numWeights does not match the number of edges in the graph.
+ */
+bool maxSpanningTree(size_t* edgeIndicesBuffer, size_t bufferSize, const MultiGraph& graph, const EdgeWeight* weights, size_t numWeights);
 
 
