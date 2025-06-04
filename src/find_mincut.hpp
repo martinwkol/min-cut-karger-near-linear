4ffb205c5bf1884eed7b4e7a7deaaf3bf5e52b27
Martin Koloseus <mkoloseus@posteo.de>
2025-06-04 14:02:18 +0200

    doc(find_mincut): add documentation for function findLikelyMinCut

--- src/find_mincut.hpp
+++ src/find_mincut.hpp
@@ -3,5 +3,11 @@
 #include "types.hpp"
 #include "graph.hpp"
 
-
+/**
+ * Compute a minimum cut of the given graph. This function finds the correct minimum cut
+ * with probability 1 - O(1 / n^d).
+ * 
+ * @param graph The graph whose minimum cut is to be computed
+ * @param d The exponent in the probability of success
+ */
 Cut findLikelyMinCut(const WeightedGraph& graph, double d);
