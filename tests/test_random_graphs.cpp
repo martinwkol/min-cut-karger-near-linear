5a226e532454e2d07aff7a2dba0ea691ea4a4083
Martin Koloseus <mkoloseus@posteo.de>
2025-06-05 12:36:15 +0200

    test(random_graph): use nearly equal instead of ==

--- tests/test_random_graphs.cpp
+++ tests/test_random_graphs.cpp
@@ -1,14 +1,14 @@
 #include <stdexcept>
-#include <iostream>
 #include "graph_generation.hpp"
 #include "stoer_wagner.hpp"
 #include "find_mincut.hpp"
+#include "test_tools.hpp"
 
 int main() {
     WeightedGraph graph = randomConnectedWeightedGraph(100, 1000, 2.0, 5.0);
     Cut stoerWagnerResult = stoerWagner(graph);
     Cut nearLinearResult = findLikelyMinCut(graph, 1.0);
-    if (stoerWagnerResult.weight != nearLinearResult.weight) {
+    if (nearlyEqual(stoerWagnerResult.weight, nearLinearResult.weight)) {
         char errorMessage[256];
         snprintf(errorMessage, 256, "Expected weight %f but got %f!\n", stoerWagnerResult.weight, nearLinearResult.weight);
         throw std::runtime_error(errorMessage);
