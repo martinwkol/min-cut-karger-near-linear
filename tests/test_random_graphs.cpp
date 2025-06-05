583c0dc93b753361313d9e8c4295581e9ecf35a5
Martin Koloseus <mkoloseus@posteo.de>
2025-06-05 14:29:38 +0200

    test(find-mincut): compare to stoer wagner for random graphs

--- tests/test_random_graphs.cpp
+++ tests/test_random_graphs.cpp
@@ -1,17 +1,26 @@
 #include <stdexcept>
+#include <algorithm>
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/matchers/catch_matchers_floating_point.hpp"
 #include "graph_generation.hpp"
 #include "stoer_wagner.hpp"
 #include "find_mincut.hpp"
-#include "test_tools.hpp"
 
-int main() {
-    WeightedGraph graph = randomConnectedWeightedGraph(100, 1000, 2.0, 5.0);
+using Catch::Matchers::WithinRel;
+
+static void compateToSW(const WeightedGraph& graph) {
     Cut stoerWagnerResult = stoerWagner(graph);
     Cut nearLinearResult = findLikelyMinCut(graph, 1.0);
-    if (nearlyEqual(stoerWagnerResult.weight, nearLinearResult.weight)) {
-        char errorMessage[256];
-        snprintf(errorMessage, 256, "Expected weight %f but got %f!\n", stoerWagnerResult.weight, nearLinearResult.weight);
-        throw std::runtime_error(errorMessage);
+    REQUIRE_THAT(nearLinearResult.weight, WithinRel(stoerWagnerResult.weight, 0.1));
+}
+
+TEST_CASE("Minimum cut on random graphs", "[mincut][mincut-general]" ) {
+    SECTION("Vertices: 10, Edges: 20, Weights: [1, 5]") {
+        for (int i = 0; i < 10; ++i) {
+            compateToSW(randomConnectedWeightedGraph(10, 50, 1.0, 5.0));
+        }
     }
-    return 0;
+    SECTION("Vertices: 50, Edges: 200, Weights: [1, 5]") {
+        compateToSW(randomConnectedWeightedGraph(50, 200, 1.0, 5.0));
+    }
 }
