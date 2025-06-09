5c7be1f3732529409b68a2101de1731dd6aec2ac
Martin Koloseus <mkoloseus@posteo.de>
2025-06-09 02:04:52 +0200

    test(two-respecting-cuts): strictly 1 respecting

--- tests/test_2_respecting_cuts.cpp
+++ tests/test_2_respecting_cuts.cpp
@@ -26,6 +26,27 @@ TEST_CASE("Find smallest 2-respecting cut", "[2-respecting]") {
         REQUIRE(cut.vertices[1] == 6);
     }
 
+    SECTION("Strictly 1-respecting #2") {
+        WeightedGraph graph(8, {
+            WeightedEdge(0, 1, 5.0), // 0
+            WeightedEdge(0, 2, 3.0), // 1
+            WeightedEdge(1, 3, 0.5), // 2
+            WeightedEdge(3, 4, 2.0), // 3
+            WeightedEdge(3, 5, 1.5), // 4
+            WeightedEdge(2, 6, 5.0), // 5
+            WeightedEdge(2, 7, 1.0), // 6
+        });
+        RootedSpanningTree rst(graph, { 0, 1, 2, 3, 4, 5, 6 }, 0);
+        Cut cut = findSmallest2RespectingCut(rst);
+        INFO(cut2string(cut));
+        REQUIRE(cut.weight == 0.5);
+        REQUIRE(cut.vertices.size() == 3);
+        std::sort(cut.vertices.begin(), cut.vertices.end());
+        REQUIRE(cut.vertices[0] == 3);
+        REQUIRE(cut.vertices[1] == 4);
+        REQUIRE(cut.vertices[2] == 5);
+    }
+
     SECTION("Strictly 2-respecting #1") {
         WeightedGraph graph(5, {
             WeightedEdge(0, 1, 100.0),      // 0
