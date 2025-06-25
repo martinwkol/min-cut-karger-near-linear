edb3b8685e33d0dd33e83116b525e490b7f2b0bf
Martin Koloseus <mkoloseus@posteo.de>
2025-06-25 14:21:00 +0200

    refactor(two-respecting-cuts): use index classes

--- src/two_respecting_cuts.cpp
+++ src/two_respecting_cuts.cpp
@@ -7,33 +7,33 @@
 
 struct S1RespResult {
     EdgeWeight weight;
-    size_t cutEdgeIndex;
+    RootedSpanningTree::EdgeIndex cutEdgeIndex;
 };
 
 struct S2RespResult {
     EdgeWeight weight;
-    size_t cutEdgeIndex1;
-    size_t cutEdgeIndex2;
+    RootedSpanningTree::EdgeIndex cutEdgeIndex1;
+    RootedSpanningTree::EdgeIndex cutEdgeIndex2;
 };
 
 static S1RespResult findSmallest1RespectingCut(
     const RootedSpanningTree& rst, 
-    const std::vector<std::vector<size_t>>& S_plus, 
-    const std::vector<std::vector<size_t>>& S_minus
+    const RootedSpanningTree::EdgeVector<std::vector<WeightedGraph::EdgeIndex>>& S_plus, 
+    const RootedSpanningTree::EdgeVector<std::vector<WeightedGraph::EdgeIndex>>& S_minus
 ) {       
     const WeightedGraph& graph = rst.graph();
     EdgeWeight weight = 0;
-    for (size_t edgeIndex : S_plus[0]) {
+    for (WeightedGraph::EdgeIndex edgeIndex : S_plus[RootedSpanningTree::EdgeIndex(0)]) {
         weight += graph.edge(edgeIndex).weight();
     }
 
     double minWeight = weight;
-    size_t rstCrossingEdgeIndex = 0;
-    for (size_t i = 1; i < rst.numEdges(); i++) {
-        for (size_t edgeIndex : S_plus[i]) {
+    RootedSpanningTree::EdgeIndex rstCrossingEdgeIndex(0);
+    for (RootedSpanningTree::EdgeIndex i(1); i < rst.numEdges(); ++i) {
+        for (WeightedGraph::EdgeIndex edgeIndex : S_plus[i]) {
             weight += graph.edge(edgeIndex).weight();
         }
-        for (size_t edgeIndex : S_minus[i]) {
+        for (WeightedGraph::EdgeIndex edgeIndex : S_minus[i]) {
             weight -= graph.edge(edgeIndex).weight();
         }
         if (weight < minWeight) {
@@ -47,36 +47,40 @@ static S1RespResult findSmallest1RespectingCut(
 
 static S2RespResult findSmallestStrictly2RespectingCut(
     const RootedSpanningTree& rst, 
-    const std::vector<std::vector<Interval>>& subsequences, 
-    const std::vector<std::vector<size_t>>& S_plus, 
-    const std::vector<std::vector<size_t>>& S_minus
+    const WeightedGraph::EdgeVector<std::vector<RootedSpanningTree::EdgeInterval>>& subsequences, 
+    const RootedSpanningTree::EdgeVector<std::vector<WeightedGraph::EdgeIndex>>& S_plus, 
+    const RootedSpanningTree::EdgeVector<std::vector<WeightedGraph::EdgeIndex>>& S_minus
 ) {
     const WeightedGraph& graph = rst.graph();
     TreeEdgeWeighter tew(rst.numEdges());
-    std::vector<bool> inSPlus0(graph.numEdges(), false);
-    for (size_t edgeIndex : S_plus[0]) {
+    WeightedGraph::EdgeVector<bool> inSPlus0(graph.numEdges(), false);
+    for (WeightedGraph::EdgeIndex edgeIndex : S_plus[RootedSpanningTree::EdgeIndex(0)]) {
         tew.nonPathAdd(graph.edge(edgeIndex).weight(), subsequences[edgeIndex]);
         inSPlus0[edgeIndex] = true;
     }
-    for (size_t i = 0; i < graph.numEdges(); i++) {
-        if (!inSPlus0[i]) {
-            tew.pathAdd(graph.edge(i).weight(), subsequences[i]);
+    for (WeightedGraph::EdgeIndex edgeIndex(0); edgeIndex < graph.numEdges(); ++edgeIndex) {
+        if (!inSPlus0[edgeIndex]) {
+            tew.pathAdd(graph.edge(edgeIndex).weight(), subsequences[edgeIndex]);
         }
     }
 
-    tew.intervalAdd(std::numeric_limits<double>::max(), { 0, 0 });
+    tew.intervalAdd(std::numeric_limits<double>::max(), 
+        { RootedSpanningTree::EdgeIndex(0), RootedSpanningTree::EdgeIndex(0) }
+    );
     double minWeight =  tew.minWeight();
-    size_t rstCrossingEdgeIndex1 = 0;
-    size_t rstCrossingEdgeIndex2 = tew.minEdgeIndex();
-    tew.intervalAdd(-std::numeric_limits<double>::max(), { 0, 0 });
+    RootedSpanningTree::EdgeIndex rstCrossingEdgeIndex1(0);
+    RootedSpanningTree::EdgeIndex rstCrossingEdgeIndex2 = tew.minEdgeIndex();
+        tew.intervalAdd(std::numeric_limits<double>::max(), 
+        { RootedSpanningTree::EdgeIndex(0), RootedSpanningTree::EdgeIndex(0) }
+    );
     
-    for (size_t i = 1; i < rst.numEdges(); i++) {
-        for (size_t edgeIndex : S_plus[i]) {
+    for (RootedSpanningTree::EdgeIndex i(1); i < rst.numEdges(); ++i) {
+        for (WeightedGraph::EdgeIndex edgeIndex : S_plus[i]) {
             double edgeWeight = graph.edge(edgeIndex).weight();
             tew.pathAdd(-edgeWeight, subsequences[edgeIndex]);
             tew.nonPathAdd(edgeWeight, subsequences[edgeIndex]);
         }
-        for (size_t edgeIndex : S_minus[i]) {
+        for (WeightedGraph::EdgeIndex edgeIndex : S_minus[i]) {
             double edgeWeight = graph.edge(edgeIndex).weight();
             tew.pathAdd(edgeWeight, subsequences[edgeIndex]);
             tew.nonPathAdd(-edgeWeight, subsequences[edgeIndex]);
@@ -97,20 +101,20 @@ static S2RespResult findSmallestStrictly2RespectingCut(
 Cut findSmallest2RespectingCut(const RootedSpanningTree& rst) {
     const WeightedGraph& graph = rst.graph();
 
-    std::vector<std::vector<Interval>> subsequences;
-    std::vector<std::vector<size_t>> S_plus(graph.numVertices());
-    std::vector<std::vector<size_t>> S_minus(graph.numVertices());
+    WeightedGraph::EdgeVector<std::vector<RootedSpanningTree::EdgeInterval>> subsequences;
+    RootedSpanningTree::EdgeVector<std::vector<WeightedGraph::EdgeIndex>> S_plus(graph.numVertices());
+    RootedSpanningTree::EdgeVector<std::vector<WeightedGraph::EdgeIndex>> S_minus(graph.numVertices());
     subsequences.reserve(graph.numEdges());
     
-    const std::vector<WeightedEdge>& graphEdges = graph.edges();
-    for (size_t i = 0; i < graphEdges.size(); i++) {
+    const WeightedGraph::EdgeVector<WeightedEdge>& graphEdges = graph.edges();
+    for (WeightedGraph::EdgeIndex i(0); i < graphEdges.size(); i++) {
         const WeightedEdge& edge = graphEdges[i];
         subsequences.push_back(rst.findVertex2VertexSubsequences(edge.endpoint(0), edge.endpoint(1)));
-        for (const Interval& subseq : subsequences.back()) {
+        for (const RootedSpanningTree::EdgeInterval& subseq : subsequences.back()) {
             S_plus[subseq.start].push_back(i);
             S_minus[subseq.end + 1].push_back(i);
         }
-        for (const Interval& subseq : subsequences.back()) {
+        for (const RootedSpanningTree::EdgeInterval& subseq : subsequences.back()) {
             if (!S_plus[subseq.start].empty() && !S_minus[subseq.start].empty() && 
                 S_plus[subseq.start].back() == i && S_minus[subseq.start].back() == i) {
                 
