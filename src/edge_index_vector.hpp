77c8f5984fbaee01d0b15279ca758ddaa2e0186d
Martin Koloseus <mkoloseus@posteo.de>
2025-06-25 12:39:41 +0200

    fix(edge-index-vector): fix types

--- src/edge_index_vector.hpp
+++ src/edge_index_vector.hpp
@@ -8,10 +8,13 @@ public:
     typedef _EdgeIndexTy edge_index;
     typedef _Ty value_type;
     typedef std::vector<value_type> vector_type;
+    typedef typename vector_type::size_type size_type;
+    typedef typename vector_type::iterator iterator;
+    typedef typename vector_type::const_iterator const_iterator;
 
     EdgeIndexVector() = default;
-    EdgeIndexVector(vector_type::size_type size) : mData(size) {}
-    EdgeIndexVector(vector_type::size_type size, const value_type& val) : mData(size, val) {}
+    EdgeIndexVector(size_type __n) : mData(__n) {}
+    EdgeIndexVector(size_type __n, const value_type& val) : mData(__n, val) {}
     EdgeIndexVector(std::initializer_list<value_type> l) : mData(l) {}
     template <typename _InputIterator>
     EdgeIndexVector(_InputIterator first, _InputIterator last) : mData(first, last) {}
@@ -19,12 +22,12 @@ public:
     value_type& operator[](edge_index idx) { return mData[idx.val]; }
     const value_type& operator[](edge_index idx) const { return mData[idx.val]; }
     
-    vector_type::size_type size() const { return mData.size(); }
+    size_type size() const { return mData.size(); }
 
-    vector_type::iterator begin() { return mData.begin(); }
-    vector_type::iterator end() { return mData.end(); }
-    vector_type::const_iterator begin() const { return mData.begin(); }
-    vector_type::const_iterator end() const { return mData.end(); }
+    iterator begin() { return mData.begin(); }
+    iterator end() { return mData.end(); }
+    const_iterator begin() const { return mData.begin(); }
+    const_iterator end() const { return mData.end(); }
 
     void push_back(const value_type& val) { mData.push_back(val); }
     void push_back(value_type&& val) { mData.push_back(std::move(val)); }
@@ -32,9 +35,9 @@ public:
     void emplace_back(Args&&... args) { mData.emplace_back(std::forward<Args>(args)...); }
     void pop_back() { mData.pop_back(); }   
 
-    void reserve(vector_type::size_type size) { mData.reserve(size); }
-    void resize(vector_type::size_type size) { mData.resize(size); }
-    void resize(vector_type::size_type size, const value_type& val) { mData.resize(size, val); }
+    void reserve(size_type __n) { mData.reserve(__n); }
+    void resize(size_type __n) { mData.resize(__n); }
+    void resize(size_type __n, const value_type& val) { mData.resize(__n, val); }
     
 
 private:
