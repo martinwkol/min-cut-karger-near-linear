237805b25e305553abb749e155786f4662e7b66d
Martin Koloseus <mkoloseus@posteo.de>
2025-06-25 12:07:49 +0200

    feat(edge-index-vector): begin, end, reserve, resize

--- src/edge_index_vector.hpp
+++ src/edge_index_vector.hpp
@@ -18,11 +18,21 @@ public:
     
     vector_type::size_type size() const { return mData.size(); }
 
+    vector_type::iterator begin() { return mData.begin(); }
+    vector_type::iterator end() { return mData.end(); }
+    vector_type::const_iterator begin() const { return mData.begin(); }
+    vector_type::const_iterator end() const { return mData.end(); }
+
     void push_back(const value_type& val) { mData.push_back(val); }
     void push_back(value_type&& val) { mData.push_back(std::move(val)); }
     template <typename... Args>
     void emplace_back(Args&&... args) { mData.emplace_back(std::forward<Args>(args)...); }
     void pop_back() { mData.pop_back(); }   
+
+    void reserve(vector_type::size_type size) { mData.reserve(size); }
+    void resize(vector_type::size_type size) { mData.resize(size); }
+    void resize(vector_type::size_type size, const value_type& val) { mData.resize(size, val); }
+    
 
 private:
     vector_type mData;
