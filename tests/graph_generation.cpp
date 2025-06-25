06be32af9b11d61ac4bf3554e5cc03de0394dd39
Martin Koloseus <mkoloseus@posteo.de>
2025-06-25 18:02:43 +0200

    fix(graph-generation): don't add same edge twice

--- tests/graph_generation.cpp
+++ tests/graph_generation.cpp
@@ -3,28 +3,54 @@
 #include "union_find.hpp"
 
 #include <vector>
+#include <set>
+#include <algorithm>
+#include <cassert>
+
+struct VertexPair {
+    VertexID v0;
+    VertexID v1;
+    bool operator<(const VertexPair& other) const { 
+        return
+            v0 < other.v0 ||
+            (v0 == other.v0 && v1 < other.v1);
+    }
+};
+
 
 WeightedGraph randomConnectedWeightedGraph(size_t numVertices, size_t numEdges, EdgeWeight minWeight, EdgeWeight maxWeight) {
-    std::uniform_int_distribution<int> vertexDistr(0, std::numeric_limits<int>::max());
-    std::uniform_real_distribution<double> weightDistr(minWeight, maxWeight);
+    assert(numEdges <= numVertices * (numVertices - 1) / 2);
+    
+    std::uniform_int_distribution<int> intDistr(0, std::numeric_limits<int>::max());
+    std::uniform_real_distribution<double> doubleDistr(minWeight, maxWeight);
 
     WeightedGraph::EdgeVector<WeightedEdge> edges;
     edges.reserve(numEdges);
 
+    std::set<VertexPair> unusedEdges;
+
+    for (VertexID endpoint0 = 1; endpoint0 < numVertices; endpoint0++) {
+        for (VertexID endpoint1 = 0; endpoint1 < endpoint0; endpoint1++) {
+            unusedEdges.insert({ endpoint0, endpoint1 });
+        }
+    }
+
     // Ensure that the graph is connected
     for (VertexID endpoint0 = 1; endpoint0 < numVertices; endpoint0++) {
-        VertexID endpoint1 = vertexDistr(randomGenerator) % endpoint0;
-        EdgeWeight weight = weightDistr(randomGenerator);
+        VertexID endpoint1 = intDistr(randomGenerator) % endpoint0;
+        EdgeWeight weight = doubleDistr(randomGenerator);
         edges.emplace_back(endpoint0, endpoint1, weight);
+        unusedEdges.erase({ endpoint0, endpoint1 });
     }
 
     // Add remaining edges
     for (size_t i = numVertices; i < numEdges; i++) {
-        VertexID endpoint0 = vertexDistr(randomGenerator) % numVertices;
-        VertexID endpoint1 = vertexDistr(randomGenerator) % (numVertices - 1);
-        if (endpoint1 >= endpoint0) endpoint1++;
-        EdgeWeight weight = weightDistr(randomGenerator);
-        edges.emplace_back(endpoint0, endpoint1, weight);
+        int unusedIdx = intDistr(randomGenerator) % unusedEdges.size();
+        auto it = unusedEdges.begin();
+        std::advance(it, unusedIdx);
+        EdgeWeight weight = doubleDistr(randomGenerator);
+        edges.emplace_back(it->v0, it->v1, weight);
+        unusedEdges.erase(it);
     }
 
     return WeightedGraph(numVertices, std::move(edges));
