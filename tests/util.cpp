e3c6c6e7297b6c762a1ee10573780b7103c1c016
Martin Koloseus <mkoloseus@posteo.de>
2025-06-26 14:13:23 +0200

    feat(tests/util): test graphs

--- tests/util.cpp
+++ tests/util.cpp
@@ -7,6 +7,67 @@
 #include <vector>
 #include <cassert>
 
+
+namespace TestGraphs {
+    TestGraph simple = { 
+        WeightedGraph(4, {
+            WeightedEdge(0, 1, 2.0),
+            WeightedEdge(0, 2, 3.0),
+            WeightedEdge(1, 3, 1.0),
+            WeightedEdge(2, 3, 4.0),
+        }),
+        Cut { { 1 }, 3.0 },
+        true
+    };
+
+    TestGraph medium = {
+        WeightedGraph(6, {
+            WeightedEdge(0, 1, 3.0),
+            WeightedEdge(0, 2, 2.0),
+            WeightedEdge(1, 2, 1.0),
+            WeightedEdge(1, 3, 2.0),
+            WeightedEdge(2, 4, 2.0),
+            WeightedEdge(3, 5, 3.0),
+            WeightedEdge(4, 5, 3.0),
+        }),
+        Cut { { 0, 1, 2 }, 4.0 },
+        true
+    };
+
+    TestGraph multipleMinCuts = {
+        WeightedGraph(4, {
+            WeightedEdge(0, 1, 1.0),
+            WeightedEdge(0, 2, 1.0),
+            WeightedEdge(0, 3, 1.0),
+            WeightedEdge(1, 2, 1.0),
+            WeightedEdge(1, 3, 1.0),
+            WeightedEdge(2, 3, 1.0),
+        }),
+        Cut { { 0 }, 3.0 },
+        false
+    };
+
+    TestGraph dense = {
+        WeightedGraph(5, {
+            WeightedEdge(0, 1, 4.0),
+            WeightedEdge(0, 2, 1.0),
+            WeightedEdge(0, 3, 6.0),
+            WeightedEdge(0, 4, 9.0),
+
+            WeightedEdge(1, 2, 8.0),
+            WeightedEdge(1, 3, 1.0),
+            WeightedEdge(1, 4, 1.0),
+
+            WeightedEdge(2, 3, 2.0),
+            WeightedEdge(2, 4, 1.0),
+            
+            WeightedEdge(3, 4, 5.0),
+        }),
+        Cut { { 1, 2 }, 10.0 },
+        true
+    };
+}
+
 Cut cutFromVertices(const WeightedGraph& graph, std::vector<VertexID>&& vertices) {
     EdgeWeight weight = 0.0;
     std::vector<bool> inVertices(graph.numVertices(), false);
@@ -23,10 +84,14 @@ Cut cutFromVertices(const WeightedGraph& graph, std::vector<VertexID>&& vertices
     return { std::move(vertices), weight };
 }
 
+void requireSameWeight(const Cut& testResult, const Cut& referenceCut) {
+    REQUIRE_THAT(testResult.weight, Catch::Matchers::WithinRel(referenceCut.weight, 0.0001));
+}
+
 void requireEqual(const Cut& testResult, const Cut& referenceCut, size_t numVertices) {
     assert(numVertices > 0);
     INFO(cut2string(testResult));
-    REQUIRE_THAT(testResult.weight, Catch::Matchers::WithinRel(referenceCut.weight, 0.0001));
+    requireSameWeight(testResult, referenceCut);
     std::vector<bool> inC1(numVertices, false);
     std::vector<bool> inC2(numVertices, false);
     for (VertexID vertex : testResult.vertices) inC1[vertex] = true;
@@ -37,6 +102,15 @@ void requireEqual(const Cut& testResult, const Cut& referenceCut, size_t numVert
         bool consistent = (sameSide && eq) || (!sameSide && !eq);
         REQUIRE(consistent);
     }
+}
+
+void checkMinCut(const TestGraph& testGraph, const Cut& testResult) {
+    if (testGraph.minCutUnique) {
+        requireEqual(testResult, testGraph.minCut, testGraph.graph.numVertices());
+    } else {
+        requireSameWeight(testResult, testGraph.minCut);
+    }
+
 }
 
 std::string graph2string(const WeightedGraph& graph) {
--- tests/util.hpp
+++ tests/util.hpp
@@ -4,7 +4,22 @@
 #include "types.hpp"
 #include "graph.hpp"
 
+struct TestGraph {
+    WeightedGraph graph;
+    Cut minCut;
+    bool minCutUnique;
+};
+
+namespace TestGraphs {
+    extern TestGraph simple;
+    extern TestGraph medium;
+    extern TestGraph multipleMinCuts;
+    extern TestGraph dense;
+}
+
 Cut cutFromVertices(const WeightedGraph& graph, std::vector<VertexID>&& vertices);
+void requireSameWeight(const Cut& testResult, const Cut& referenceCut);
 void requireEqual(const Cut& testResult, const Cut& referenceCut, size_t numVertices);
+void checkMinCut(const TestGraph& testGraph, const Cut& testResult);
 std::string graph2string(const WeightedGraph& graph);
 std::string cut2string(const Cut& cut);
