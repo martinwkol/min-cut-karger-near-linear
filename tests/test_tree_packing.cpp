ba4300c30d81448198e2fea7f8ef454216c188f7
Martin Koloseus <mkoloseus@posteo.de>
2025-06-26 13:49:50 +0200

    test&fix(test-tree-packing): more (& fixed) tests

--- tests/test_tree_packing.cpp
+++ tests/test_tree_packing.cpp
@@ -32,17 +32,85 @@ static void checkPackingSize(const TreePacking& packing, double epsilon) {
     REQUIRE(packing.packingWeight() >= (1 - epsilon) * minCut.weight / 2);
 }
 
+static void checkFromWeightedGraph(const WeightedGraph& graph, double multApproxEps, double packingEps) {
+    MultiGraph multiGraph = graph.approxAsMultiGraph(multApproxEps);
+    TreePacking packing(multiGraph, packingEps);
+    checkIfValid(packing);
+    checkPackingSize(packing, packingEps);
+}
+
 TEST_CASE("Compute Tree packing", "[tree-packing]") {
     randomGenerator.seed(123456);
 
-    SECTION("Test random multigraphs") {
-        double packingEpsilon = 1.0 / 5.0;
-        for (int i = 0; i < 10; ++i) {
-            WeightedGraph weightedGraph = randomConnectedWeightedGraph(10, 50, 1.0, 5.0);
-            MultiGraph multiGraph = weightedGraph.approxAsMultiGraph(0.01);
-            TreePacking packing(multiGraph, packingEpsilon);
-            checkIfValid(packing);
-            checkPackingSize(packing, packingEpsilon);
+    SECTION("Simple graph") {
+        WeightedGraph graph(4, {
+            WeightedEdge(0, 1, 2.0),
+            WeightedEdge(0, 2, 3.0),
+            WeightedEdge(1, 3, 1.0),
+            WeightedEdge(2, 3, 4.0),
+        });
+        checkFromWeightedGraph(graph, 0.01, 1.0 / 5.0);
+    }
+
+    SECTION("Medium graph") {
+        WeightedGraph graph(6, {
+            WeightedEdge(0, 1, 3.0),
+            WeightedEdge(0, 2, 2.0),
+            WeightedEdge(1, 2, 1.0),
+            WeightedEdge(1, 3, 2.0),
+            WeightedEdge(2, 4, 2.0),
+            WeightedEdge(3, 5, 3.0),
+            WeightedEdge(4, 5, 3.0),
+        });
+        checkFromWeightedGraph(graph, 0.01, 1.0 / 5.0);
+    }
+
+    SECTION("Multiple min-cuts") {
+        WeightedGraph graph(4, {
+            WeightedEdge(0, 1, 1.0),
+            WeightedEdge(0, 2, 1.0),
+            WeightedEdge(0, 3, 1.0),
+            WeightedEdge(1, 2, 1.0),
+            WeightedEdge(1, 3, 1.0),
+            WeightedEdge(2, 3, 1.0),
+        });
+        checkFromWeightedGraph(graph, 0.01, 1.0 / 5.0);
+    }
+
+    SECTION("Dense graph") {
+        WeightedGraph graph(5, {
+            WeightedEdge(0, 1, 4.0),
+            WeightedEdge(0, 2, 1.0),
+            WeightedEdge(0, 3, 6.0),
+            WeightedEdge(0, 4, 9.0),
+
+            WeightedEdge(1, 2, 8.0),
+            WeightedEdge(1, 3, 1.0),
+            WeightedEdge(1, 4, 1.0),
+
+            WeightedEdge(2, 3, 2.0),
+            WeightedEdge(2, 4, 1.0),
+            
+            WeightedEdge(3, 4, 5.0),
+        });
+        checkFromWeightedGraph(graph, 0.01, 1.0 / 5.0);
+    }
+
+    SECTION("Random sparse graphs, Weights: [1, 5]") {
+        for (size_t numVertices = 10; numVertices <= 40; numVertices += 10) {
+            checkFromWeightedGraph(
+                randomConnectedWeightedGraph(numVertices, numVertices * 3 / 2, 1.0, 5.0),
+                0.01, 1.0 / 5.0
+            );
+        }
+    }
+
+    SECTION("Random dense graphs, Weights: [1, 5]") {
+        for (size_t numVertices = 10; numVertices <= 25; numVertices += 5) {
+            checkFromWeightedGraph(
+                randomConnectedWeightedGraph(numVertices, numVertices * numVertices / 3, 1.0, 5.0),
+                0.01, 1.0 / 5.0
+            );
         }
     }
 }
