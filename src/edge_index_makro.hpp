63bd7e62106598172734b96690086725ea094300
Martin Koloseus <mkoloseus@posteo.de>
2025-06-27 11:28:57 +0200

    opt(edge-index-makro): use constexpr

--- src/edge_index_makro.hpp
+++ src/edge_index_makro.hpp
@@ -4,24 +4,24 @@
 class CLASS_NAME { \
 public: \
     CLASS_NAME() = default;                         \
-    explicit CLASS_NAME(size_t val) : mVal(val) {}  \
+    constexpr explicit CLASS_NAME(size_t val) : mVal(val) {}  \
     \
-    bool operator==(const CLASS_NAME& index) { return mVal == index.mVal; } \
-    bool operator!=(const CLASS_NAME& index) { return mVal != index.mVal; } \
-    bool operator<(const CLASS_NAME& index) { return mVal < index.mVal; }   \
-    bool operator>(const CLASS_NAME& index) { return mVal > index.mVal; }   \
-    bool operator<=(const CLASS_NAME& index) { return mVal <= index.mVal; } \
-    bool operator>=(const CLASS_NAME& index) { return mVal >= index.mVal; } \
+    constexpr bool operator==(const CLASS_NAME& index) { return mVal == index.mVal; } \
+    constexpr bool operator!=(const CLASS_NAME& index) { return mVal != index.mVal; } \
+    constexpr bool operator<(const CLASS_NAME& index) { return mVal < index.mVal; }   \
+    constexpr bool operator>(const CLASS_NAME& index) { return mVal > index.mVal; }   \
+    constexpr bool operator<=(const CLASS_NAME& index) { return mVal <= index.mVal; } \
+    constexpr bool operator>=(const CLASS_NAME& index) { return mVal >= index.mVal; } \
     \
-    bool operator<(size_t v) { return mVal < v; }    \
-    bool operator>(size_t v) { return mVal > v; }    \
-    bool operator<=(size_t v) { return mVal <= v; }  \
-    bool operator>=(size_t v) { return mVal >= v; }  \
+    constexpr bool operator<(size_t v) { return mVal < v; }    \
+    constexpr bool operator>(size_t v) { return mVal > v; }    \
+    constexpr bool operator<=(size_t v) { return mVal <= v; }  \
+    constexpr bool operator>=(size_t v) { return mVal >= v; }  \
     \
-    CLASS_NAME operator+(size_t i) const { return CLASS_NAME(mVal + i); }   \
-    CLASS_NAME& operator+=(size_t i) { mVal += i; return *this; }           \
-    CLASS_NAME& operator++() { ++mVal; return *this; }                      \
-    CLASS_NAME operator++(int) { auto tmp = *this; ++(*this); return tmp; } \
+    constexpr CLASS_NAME operator+(size_t i) const { return CLASS_NAME(mVal + i); }   \
+    constexpr CLASS_NAME& operator+=(size_t i) { mVal += i; return *this; }           \
+    constexpr CLASS_NAME& operator++() { ++mVal; return *this; }                      \
+    constexpr CLASS_NAME operator++(int) { auto tmp = *this; ++(*this); return tmp; } \
     \
     constexpr size_t get() const { return mVal; } \
     \
